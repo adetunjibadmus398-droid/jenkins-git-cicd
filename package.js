a24c4a46e9979b30dc5ec2d262ba282a1b229b75
adetunjibadmus398-droid <adetunjibadmus398@gmail.com>
2026-06-28 14:35:07 -0400

    Initialize package.json for simple Node.js app

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "simple-node-app",
+  "version": "1.0.0",
+  "description": "Simple Node.js App",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js",
+    "test": "node -e \"console.log('Tests Passed')\""
+  },
+  "author": "",
+  "license": "MIT",
+  "dependencies": {
+    "express": "^4.21.0"
+  }
+}
